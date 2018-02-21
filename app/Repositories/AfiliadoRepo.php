23842afac193ddb9e79a508a75609b2fe3365986
joaquin3684 <joaquinmazoud@gmail.com>
2018-02-21 12:31:46 -0300

    lkajsdflj

--- app/Repositories/AfiliadoRepo.php
+++ app/Repositories/AfiliadoRepo.php
@@ -24,4 +24,12 @@ class AfiliadoRepo extends Repositorio
         $obj = $this->gateway->with('obraSocial')->find($id);
         return $this->mapper->map($obj);
     }
+
+    public function all()
+    {
+        $obj = $this->gateway->with('obraSocial')->get();
+        return $obj->map(function($obj){
+            return $this->mapper->map($obj);
+        });
+    }
 }
