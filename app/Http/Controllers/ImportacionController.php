3ebf60611447d6b3c7116cdf00fd67ee3066502e
joaquin3684 <joaquinmazoud@gmail.com>
2018-06-11 20:22:29 -0300

    aslkdfj

--- app/Http/Controllers/ImportacionController.php
+++ app/Http/Controllers/ImportacionController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Repositories\AfiliadoRepo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ImportacionController extends Controller
 {
     public function importarAfiliados(Request $request)
     {
-        foreach ($request->all() as $afiliado)
-        {
-            $repo = new AfiliadoRepo();
-            $repo->create($afiliado);
+        DB::transaction(function() use ($request) {
 
-        }
+            foreach ($request->all() as $afiliado) {
+                $repo = new AfiliadoRepo();
+                $repo->create($afiliado);
+
+            }
+        });
     }
 }
