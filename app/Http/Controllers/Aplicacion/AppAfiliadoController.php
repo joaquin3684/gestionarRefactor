142e1cdff0fe28635c9b32870e494695970cd3a0
joaquin3684 <joaquinmazoud@gmail.com>
2018-10-09 12:05:10 -0300

    ljasdlfj

--- app/Http/Controllers/Aplicacion/AppAfiliadoController.php
+++ app/Http/Controllers/Aplicacion/AppAfiliadoController.php
@@ -17,10 +17,10 @@ class AppAfiliadoController extends Controller
     public function __construct()
     {
         // $this->obsUser = collect([1,2,3])
-        $service = new UserFromToken();
+        /*$service = new UserFromToken();
         $this->userRepo = new UserRepo();
         $this->user = $service->getUser();
-        $this->obsUser = $this->user->obrasSociales->map(function($obraSocial){ return $obraSocial->ID;});
+        $this->obsUser = $this->user->obrasSociales->map(function($obraSocial){ return $obraSocial->ID;});*/
     }
     public function modificarEmail(Request $request)
     {
