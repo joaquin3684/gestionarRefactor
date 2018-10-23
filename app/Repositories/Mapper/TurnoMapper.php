2c6671f11119cfec53ae293eefecb8a269529bef
jaccinelli <juanpabloaccinelli@gmail.com>
2018-10-23 14:17:04 -0300

    juanjii

--- app/Repositories/Mapper/TurnoMapper.php
+++ app/Repositories/Mapper/TurnoMapper.php
@@ -14,7 +14,7 @@ class TurnoMapper
     }
     public function map($objeto)
     {
-        $turno =  new \App\Domain\Turno($objeto->IDT, $objeto->FECHAT, $objeto->HORAT, $objeto->CONFIRMACION, $objeto->MEDICOASIGNADO, $objeto->MOTIVOT);
+        $turno =  new \App\Domain\Turno($objeto->IDT, $objeto->FECHAT, $objeto->HORAT, $objeto->CONFIRMACION, $objeto->MEDICOASIGNADO, $objeto->MOTIVOT, $objeto->FECHACREACION);
         if($objeto->relationLoaded('climed') && $objeto->climed != null)
         {
             $climed = $this->climedMapper->map($objeto->climed);
