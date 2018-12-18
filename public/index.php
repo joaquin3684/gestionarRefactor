6bb47a9aa72df056a422fd4fce67f21fafffba02
jaccinelli <juanpabloaccinelli@gmail.com>
2018-12-18 14:12:10 -0300

    juan

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Osstwell <taylor@laravel.com>
+ * @author   Taylor Ossstwell <taylor@laravel.com>
  */
 
 /*
